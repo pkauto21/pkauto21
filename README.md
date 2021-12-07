146676013b85adaa7767640bd50b9de71990e591
pkauto21 <nonstoppurushoth@gmail.com>
2021-12-07 08:50:53 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+1. Git Repo Cloning
+
+```bash
+## clone this repo to a local directory
+git clone https://github.com/<your-username>/exercise2-cypress.git
+
+## cd into the cloned repo
+cd exercise2-cypress
+
+## install the node_modules
+npm install
+
+## start the local webserver
+npm start
+```
+
+The `npm start` script will spawn a webserver on port `8080` which hosts the Kitchen Sink App.
+
+You can verify this by opening your browser and navigating to: [`http://localhost:8080`](http://localhost:8080)
+
+We are now ready to run Cypress tests.
+
+2. Run Cypress Test locally
+```bash
+## launch the cypress test runner
+npm run test
+```
---
+++ cypress/support/commands.js
@@ -0,0 +1,50 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => {...})
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+// Custom commands
+// Login with valid credentials
+Cypress.Commands.add('login', (email, password) => {
+  cy.fixture('example').then(function (data) {
+    this.data = data
+    //        Valid user credentials. email and password picked from fixtures
+    cy.visit("https://www.saucedemo.com/")
+    cy.get('[data-test="username"]').type(this.data.email1)
+    cy.get('[data-test="password"]').type(this.data.password1)
+    cy.get('[data-test="login-button"]').click()
+  })
+})
+
+
+Cypress.Commands.add('sort', () => {
+  Cypress.log({
+    message: 'sortByPriceHighToLow',
+    name: 'Sort the list by Price High to Low'
+  });
+  cy.get('.select_container')
+  cy.get('[data-test="product_sort_container"]').select('Price (high to low)')
+})
+
